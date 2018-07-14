28d4ee6937231be4dda8d5d1953431230f66e694
Marius Hegele <marius.hegele@ibm.com>
2018-07-14 09:39:52 +0200

    Added a blacklist file for additional sites, changed killing browsers

--- gsd.sh
+++ gsd.sh
@@ -2,8 +2,10 @@
 
 # edit this list, or set GSD_SITES to add your custom sites
 BASE_SITES="reddit.com forums.somethingawful.com somethingawful.com digg.com break.com news.ycombinator.com infoq.com bebo.com twitter.com facebook.com blip.com youtube.com vimeo.com delicious.com flickr.com friendster.com hi5.com linkedin.com livejournal.com meetup.com myspace.com plurk.com stickam.com stumbleupon.com yelp.com slashdot.com"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+echo $DIR
 
-SITES="$GSD_SITES $BASE_SITES"
+SITES="$GSD_SITES $BASE_SITES $(<$DIR/blacklist)"
 
 HOSTFILE="/etc/hosts"
 
@@ -16,19 +18,24 @@ fi
 sed -i -e '/#gsd$/d' $HOSTFILE
 
 # write hosts file if 'work' mode
+# on switch back to play no kill is necessary
 if [ "$1" != "--play" ]
 then
     for SITE in $SITES; do
-	    echo -e "127.0.0.1\t$SITE\t#gsd" >> $HOSTFILE
-	    echo -e "127.0.0.1\twww.$SITE\t#gsd" >> $HOSTFILE
+	    echo "127.0.0.1\t$SITE\t#gsd" >> $HOSTFILE
+	    echo "127.0.0.1\twww.$SITE\t#gsd" >> $HOSTFILE
     done
     echo "work mode enabled, run with --play to disable"
-fi
 
-if [ "$(uname -s)" == "Darwin" ]; then
-	dscacheutil -flushcache
-elif [ "$(uname -s)" == "Linux" ]; then
-	for BROWSER in chromium firefox iceweasel; do
-		killall $BROWSER > /dev/null 2>&1
-	done
-fi  
+	if [ "$(uname -s)" == "Darwin" ]; then
+		dscacheutil -flushcache
+		for BROWSER in chromium Google Chrome firefox iceweasel; do
+			pkill -s TERM -f $BROWSER
+			#killall -9 $BROWSER > /dev/null 2>&1
+		done
+	elif [ "$(uname -s)" == "Linux" ]; then
+		for BROWSER in chromium firefox iceweasel; do
+			killall $BROWSER > /dev/null 2>&1
+		done
+	fi
+fi
