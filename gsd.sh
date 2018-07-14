a076c3d6968586217eefaa3912bc8a2bc45b6ca9
Marius Hegele <marius.hegele@ibm.com>
2018-07-14 20:45:18 +0200

    Added an option to take a break of 5 minutes after each session

--- gsd.sh
+++ gsd.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # edit this list, or set GSD_SITES to add your custom sites
 BASE_SITES="reddit.com forums.somethingawful.com somethingawful.com digg.com break.com news.ycombinator.com infoq.com bebo.com twitter.com facebook.com blip.com youtube.com vimeo.com delicious.com flickr.com friendster.com hi5.com linkedin.com livejournal.com meetup.com myspace.com plurk.com stickam.com stumbleupon.com yelp.com slashdot.com"
@@ -8,6 +8,19 @@ echo $DIR
 SITES="$GSD_SITES $BASE_SITES $(<$DIR/blacklist)"
 
 HOSTFILE="/etc/hosts"
+REDIRECT_SERVER="127.0.0.1"
+
+# expects as first argument the amount of seconds to count down
+countdown() {
+	now=$(date +%s)
+	end=$((now + $1))
+	while (( now < end )); do   
+		printf "%s\r" "$(date -u -j -f %s $((end - now)) +%T)"  
+		sleep 0.25  
+		now=$(date +%s)
+	done
+	echo
+}
 
 if [ ! -w $HOSTFILE ]; then
     echo "cannot write to $HOSTFILE, try running with sudo"
@@ -17,19 +30,24 @@ fi
 # clean up previous entries from /etc/hosts
 sed -i -e '/#gsd$/d' $HOSTFILE
 
-# write hosts file if 'work' mode
-# on switch back to play no kill is necessary
+# while working iterate over session cycle (25-5)
 if [ "$1" != "--play" ]
 then
+	working=true
+fi
+
+# write hosts file if 'work' mode
+# on switch back to play no kill is necessary
+while [ "$working" = "true" ]
+do
     for SITE in $SITES; do
-	    echo "127.0.0.1\t$SITE\t#gsd" >> $HOSTFILE
-	    echo "127.0.0.1\twww.$SITE\t#gsd" >> $HOSTFILE
+	    echo "$REDIRECT_SERVER\t$SITE\t#gsd" >> $HOSTFILE
+	    echo "$REDIRECT_SERVER\twww.$SITE\t#gsd" >> $HOSTFILE
     done
-    echo "work mode enabled, run with --play to disable"
 
 	if [ "$(uname -s)" == "Darwin" ]; then
 		dscacheutil -flushcache
-		for BROWSER in chromium Chrome firefox iceweasel; do
+		for BROWSER in chromium Chrome iceweasel; do
 			pkill -s TERM -f $BROWSER
 			#killall -9 $BROWSER > /dev/null 2>&1
 		done
@@ -38,24 +56,40 @@ then
 			killall $BROWSER > /dev/null 2>&1
 		done
 	fi
-fi
 
-if [ "$1" == "-t" ]
-then
-	# second argument should be time in minutes
-	seconds=$(($2*60))
-	echo "Pomodoro started - Let's get to work!"
-	now=$(date +%s)
-	end=$((now + $seconds))
-	while (( now < end )); do   
-		printf "%s\r" "$(date -u -j -f %s $((end - now)) +%T)"  
-		sleep 0.25  
-		now=$(date +%s)
-	done  
-	echo
+	# Adds a Countdown/Session timer that is activatable by -t <minutes>
+	if [ "$1" == "-t" ]
+	then
+		# second argument should be time in minutes
+		seconds=$(($2*60))
+		echo "Session started - Let's get to work!"
+		countdown $seconds
 
-	# after countdown ring a bell and clean up the hostfile again (Mac OS X Specific)
-	sed -i -e '/#gsd$/d' $HOSTFILE
-	printf \\a
-	osascript -e 'tell app "System Events" to display dialog "Pomodoro Finished!"'
-fi
+		# after countdown ring a bell and clean up the hostfile again (Mac OS X Specific)
+		sed -i -e '/#gsd$/d' $HOSTFILE
+		printf \\a
+
+		# use AppleScript to get a user decision (continue working after break or finish)
+		osascript -e 'tell app "System Events" to set answer to the button returned of (display dialog "Session Finished! Take a break?" buttons {"No", "Break"} default button 2)' | tail -n 1 > /tmp/gsd_out
+		decision=$(cat /tmp/gsd_out)
+
+		# if the decision was to take a break countdown 5 minutes, alert and go back to working
+		if [ "$decision" == "Break" ]
+		then
+			echo "Break started - Lean back!"
+			countdown $((5*60))
+			printf \\a
+			osascript -e 'tell app "System Events" to set answer to the button returned of (display dialog "Break Finsished! Go on working?" buttons {"No", "Yes"} default button 2)' | tail -n 1 > /tmp/gsd_out
+			decision2=$(cat /tmp/gsd_out)
+
+			if [ "$decision2" == "No" ]
+			then
+				working=false
+			fi
+		else
+			working=false
+		fi
+	else
+		working=false
+	fi
+done
